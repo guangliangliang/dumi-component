f15555ad6e1e92e2f178dd9151a299061c37f478
menglh18 <menglh18@chinaunicom.cn>
2020-12-29 09:32:47 +0800

    feax: add document

--- .umirc.ts
+++ .umirc.ts
@@ -1,3 +1,11 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Autor: unicom
+ * @Date: 2020-12-28 17:08:16
+ * @LastEditors: unicom
+ * @LastEditTime: 2020-12-28 17:54:25
+ */
 import { defineConfig } from 'dumi';
 
 const repo = 'dumi-template';
@@ -5,15 +13,16 @@ const repo = 'dumi-template';
 export default defineConfig({
   title: repo,
   favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+    'https://gitee.com/guangliangliang/images/raw/master/img/favicon.ico', //配置 favicon 地址（href 属性）。
+  logo: 'https://gitee.com/guangliangliang/images/raw/master/img/favicon.ico', //配置 favicon 地址（href 属性）。
+  // logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  // description:null,//配置文档的介绍，会显示在侧边栏菜单标题的下方，仅 doc 模式下可用。
   outputPath: 'docs-dist',
   mode: 'site',
   hash: true,
   exportStatic: {},
   // Because of using GitHub Pages
-  base: `/${repo}/`,
+  base: `/${repo}/`, //设置路由前缀，通常用于部署到非根目录。
   publicPath: `/${repo}/`,
   navs: [
     null,
@@ -22,5 +31,9 @@ export default defineConfig({
       path: 'https://github.com/umijs/dumi-template',
     },
   ],
+  theme: {
+    '@c-heading': '#454d64',
+    //主题颜色变量名称参照 https://github.com/umijs/dumi/blob/master/packages/theme-default/src/style/variables.less
+  },
   // more config: https://d.umijs.org/config
 });
